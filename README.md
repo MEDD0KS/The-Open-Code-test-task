66f61e51aa2594355bcf5f3b40d92f63a3064d79
MEDD0KS <serega.volkov1234@gmail.com>
2024-05-04 17:32:13 +0400

    Update readmy, added comments

--- README.md
+++ README.md
@@ -1,58 +1,23 @@
-| Supported Targets | ESP32 | ESP32-C2 | ESP32-C3 | ESP32-C6 | ESP32-H2 | ESP32-S2 | ESP32-S3 |
-| ----------------- | ----- | -------- | -------- | -------- | -------- | -------- | -------- |
+| Проверенные платформы | ESP32-C6 |
+| --------------------- | -------- | 
 
-# UART Asynchronous Example with Separate Receive and Transfer Tasks
+# Тестовое задание для компании "Открытый код".
 
-(See the README.md file in the upper level 'examples' directory for more information about examples.)
+## Алгоритм работы:
+1. Иницициализация перефериии - управляемые пины, UART, таймер
+2. Создание задачи приемника UART
+3. Ожидание поступления байтов, проверка посылки на соответствие заданному формату сообщений
+4. Создается задача на окрытие замка и установку частоты мерцания светодиода
+5. В прерывании таймера переключается состояние светодиода, что обеспечивает мерцание светодиода с указанными времеными промежутками
+6. При завершении порты закрываются, счетнные переменные сбрасываются
 
-This example demonstrates how two asynchronous tasks can use the same UART interface for communication. One can use
-this example to develop more complex applications for serial communication.
+## Формат посылки
 
-The example starts two FreeRTOS tasks:
-1. The first task periodically transmits `Hello world` via the UART.
-2. The second task task listens, receives and prints data from the UART.
+|  0x02  |  0x33  |  0x01  |   ID   | Command | Unlock time | Flash time |  0x33  |  0x03  |
+|--------|--------|--------|--------|---------|-------------|------------|--------|--------|
+| 1 байт | 1 байт | 1 байт | 1 байт | 1 байт  | 2 байта     | 2 байта    | 1 байт | 1 байт |
 
-## How to use example
+В отладочном Serialport esp32 выводится дополнительная информация в виде логирования.
+В настройках SDK ESP_IDF параметр configTICK_RATE_HZ установлен в 20000 (Гц)
 
-### Hardware Required
 
-The example can be run on any commonly available development board, that is based on the Espressif SoC. You will need a
-USB cable to connect the development board to a computer, and a simple one-wire cable for shorting two pins of the board.
-
-### Setup the Hardware
-
-The `RXD_PIN` and `TXD_PIN` which are configurable in the code (by default `GPIO4` and `GPIO5`) need to be shorted in
-order to receive back the same data which were sent out.
-
-### Configure the project
-
-```
-idf.py menuconfig
-```
-
-### Build and Flash
-
-Build the project and flash it to the board, then run monitor tool to view serial output:
-
-```
-idf.py -p PORT flash monitor
-```
-
-(To exit the serial monitor, type ``Ctrl-]``.)
-
-See the Getting Started Guide for full steps to configure and use ESP-IDF to build projects.
-
-## Example Output
-
-You will receive the following repeating output from the monitoring console:
-```
-...
-I (3261) TX_TASK: Wrote 11 bytes
-I (4261) RX_TASK: Read 11 bytes: 'Hello world'
-I (4261) RX_TASK: 0x3ffb821c   48 65 6c 6c 6f 20 77 6f  72 6c 64                 |Hello world|
-...
-```
-
-## Troubleshooting
-
-If you do not see any output from `RX_TASK` then check if you have the `RXD_PIN` and `TXD_PIN` pins shorted on the board.
--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -1,10 +1,6 @@
-/* UART asynchronous example, that uses separate RX and TX tasks
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/* Тестовое задание для компании "Открытый код"
+    Выполнил Волков Сергей
+    Задание приведено в файле task-embedded-2
 */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -29,9 +25,9 @@ struct channel_task_args
 };
 
 //поля для таймера
-static uint32_t flash_time_timer_value[5] = {1 , 1 , 1, 1 , 1};
-static uint16_t unique_counter[5] = {0, 0, 0, 0 , 0};
-static bool pin_state_flag[5] = {false, false, false, false, false};
+static uint32_t flash_time_timer_value[5] = {1, 1, 1, 1, 1}; //поле значений для того до скольки считать 
+static uint16_t unique_counter[5] = {0, 0, 0, 0, 0}; //счетчик таймера для каждого канала
+static bool pin_state_flag[5] = {false, false, false, false, false}; //состояние пина светодиода
 
 //Флаги каналов
 bool channelLockFlag[5] = {false, false, false, false, false};
@@ -60,13 +56,13 @@ static gpio_num_t led_pins_array[5] = {CHANNEL_1_LED_PIN, CHANNEL_2_LED_PIN, CHA
 // Прерывание таймера
 static bool IRAM_ATTR gptimer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
 { 
-    //Проверка флага каналов
+    //Проверка флагов для каждого канала
     if(channelLockFlag[0]){   
-        //Блинк по достижении заданного счета             
+        //Проверка заданного времени и блинк по достижении заданного счета             
         if (unique_counter[0] == flash_time_timer_value[0]/2){
             //ESP_DRAM_LOGW("timer0", "Hardware timer channel 1 alarm! Count: %d", unique_counter[0]);
             pin_state_flag[0] = !pin_state_flag[0];
-            gpio_set_level(CHANNEL_1_LED_PIN, pin_state_flag[0]);            
+            gpio_set_level(led_pins_array[0], pin_state_flag[0]);            
             unique_counter[0] = 0;
         }
         unique_counter[0]++;        
@@ -109,6 +105,9 @@ static bool IRAM_ATTR gptimer_alarm_callback(gptimer_handle_t timer, const gptim
 
 void init(void) {
 
+    static const char *INIT_TAG = "INIT";
+    esp_log_level_set(INIT_TAG, ESP_LOG_INFO);
+
     //Инициализация пинов управления
     gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_DISABLE;
@@ -121,6 +120,8 @@ void init(void) {
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
+
+    ESP_LOGI(INIT_TAG, "Hardware gpio init");
 
     //Инициализация UART    
     const uart_config_t uart_config = {
@@ -135,153 +136,130 @@ void init(void) {
     uart_param_config(UART_NUM_1, &uart_config);
     uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);    
 
+    ESP_LOGI(INIT_TAG, "UART created, speed: %d", uart_config.baud_rate);
+
     //Инициализация таймера
     const gptimer_config_t timer_config ={
         .clk_src = GPTIMER_CLK_SRC_DEFAULT,
         .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = 1000000
+        .resolution_hz = 1000000 // частота счета 1 МГц
     };
     gptimer_new_timer(&timer_config, &gptimer);
 
-  // Подключаем функцию обратного вызова
+  // Добавление callback таймера в обработку
     gptimer_event_callbacks_t cb_config = {
         .on_alarm = gptimer_alarm_callback,
     };
-    gptimer_register_event_callbacks(gptimer, &cb_config, NULL);
-
-    // Задаем начальное значение счетчика таймера (не обязательно)
-    gptimer_set_raw_count(gptimer, 0);
+    gptimer_register_event_callbacks(gptimer, &cb_config, NULL);   
 
     // Задаем параметры счетчика таймера
     gptimer_alarm_config_t alarm_config = {
-        .alarm_count = 1000,                   // Конечное значение счетчика = 0.001 секунды
-        .reload_count = 0,                        // Значение счетчика при автосбросе
-        .flags.auto_reload_on_alarm = true,       // Автоперезапуск счетчика таймера разрешен
+        .alarm_count = 500,                   // Конечное значение счетчика = 0.5 милисекунды
+        .reload_count = 0,                        // Значение счетчика при сбросе
+        .flags.auto_reload_on_alarm = true,       // Автоперезапуск таймера
     };
     gptimer_set_alarm_action(gptimer, &alarm_config);
 
+    gptimer_set_raw_count(gptimer, 0);
     // Разрешаем прерывания для данного таймера
     gptimer_enable(gptimer);
 
-    // Запускаем таймер
-    //gptimer_start(gptimer);
-    ESP_LOGI("main", "Hardware timer created");
-
+    // Запускаем таймер   
     gptimer_start(gptimer);
+
+    ESP_LOGI(INIT_TAG, "Hardware timer created");
 }
 
 void task_channel(void *arg){    
 
+    //Инфо в логировании
     static const char *CHANNEL_TASK_TAG = "CHANNEL_TASK";
     esp_log_level_set(CHANNEL_TASK_TAG, ESP_LOG_INFO);
 
+    //Перенос аргументов в локальные поля
     struct channel_task_args *param = (struct channel_task_args*) arg; 
-
-    if(channelLockFlag[param->channel - 1]) {
-        ESP_LOGI(CHANNEL_TASK_TAG, "Channel already work");
-    }
-    else{        
-        ESP_LOGI(CHANNEL_TASK_TAG, "channel: %d", param->channel);
-        ESP_LOGI(CHANNEL_TASK_TAG, "unlock_time: %d", param->_unlock_time);
-        ESP_LOGI(CHANNEL_TASK_TAG, "flash_time: %d", param->_flash_time);
-
-        ESP_LOGI(CHANNEL_TASK_TAG, "Task created%d", param->channel);
-
-
-        flash_time_timer_value[param->channel - 1] = param->_flash_time * 2;
-        channelLockFlag[param->channel - 1] = true;      
-
-        //if(gptimer_get)
-        //gptimer_start(gptimer);
-
-        gpio_set_level(lock_pins_array[param->channel - 1], 1);        
-
-        uint8_t curr_channel = param->channel;
-
-        vTaskDelay(param->_unlock_time / portTICK_PERIOD_MS);
-
-        //ESP_LOGI(CHANNEL_TASK_TAG, "Task finished %d", param->channel);
-
-        //vPortEnterCritical();    
-
-        ESP_LOGI(CHANNEL_TASK_TAG, "Task finished %d", curr_channel);
-
-        channelLockFlag[curr_channel-1] = false;
-
-        //ESP_LOGI(CHANNEL_TASK_TAG, "State %d", channelLockFlag[param->channel - 1]);
-
-        gpio_set_level(lock_pins_array[curr_channel-1], 0);
-
-        gpio_set_level(led_pins_array[curr_channel-1], 0);
-
-        //vPortExitCritical(); 
-
-        //gptimer_stop(gptimer);
-
-        
-    }
-
+    uint8_t curr_channel = param->channel;
+    uint16_t curr_unlock = param->_unlock_time;
+    uint16_t curr_flash = param->_flash_time;
     
+    //Инфо
+    ESP_LOGI(CHANNEL_TASK_TAG, "channel: %d", curr_channel);
+    ESP_LOGI(CHANNEL_TASK_TAG, "unlock_time: %d", curr_unlock);
+    ESP_LOGI(CHANNEL_TASK_TAG, "flash_time: %d", curr_flash);
+
+    ESP_LOGI(CHANNEL_TASK_TAG, "Task created%d", curr_channel);
+
+    //Запись в поле времени до скольки считать
+    flash_time_timer_value[curr_channel - 1] = curr_flash * 2;
+    //Блокирование канала
+    channelLockFlag[curr_channel - 1] = true;      
+    //Открытие замка
+    gpio_set_level(lock_pins_array[curr_channel - 1], 1);        
+    //Ожидание времени
+    vTaskDelay(curr_unlock / portTICK_PERIOD_MS);
+
+    //Завершение задачи
+    ESP_LOGI(CHANNEL_TASK_TAG, "Task finished %d", curr_channel);
+    //Закрытие замка и отключение светодиода
+    gpio_set_level(lock_pins_array[curr_channel-1], 0);
+    gpio_set_level(led_pins_array[curr_channel-1], 0);
+    unique_counter[curr_channel - 1] = 0;
+    //Снятие флага занятости
+    channelLockFlag[curr_channel-1] = false;
 
     vTaskDelete(NULL);
 }
 
 static void rx_task(void *arg)
 {
+    //Инфо в логировании
     static const char *RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    //Выделение памяти для приема
     uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    //цикл применика
     while (1) {
-        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        //количество поступивших байт и занесениеих в выделенную память data
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);        
         if (rxBytes > 0) {
             data[rxBytes] = 0;
+
             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
             ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            //Проверяем посылку на соответствии информационному пакету
+            //0. Экранирующий символ
+            //1-2. Заголовок
+            //9-10. Окончание
             if (data[0]==0x02 && data[1]==0x33 &&data[2]==0x01 && data[9]==0x33 && data[10]==0x03)
             {
                 ESP_LOGI(RX_TASK_TAG, "Success get message");
-
+                //ID канала 1-5, если иное - игнорируем
                 if(data[3] > 0 && data[3] <= MAX_CHANNEL_NUMBER){
                     ESP_LOGI(RX_TASK_TAG, "Channel %d select", data[3]);
+                    //Проверяем доступность канала, если занят - посылка игнорируется
+                    if(!channelLockFlag[data[3] - 1]) {
+                        //проверка команды откртытия, иные команды не указаны
+                        if(data[4] == 2){           
+                            //Примем структуру LSB для данных размером 2 байта  
+                            uint16_t unlock_time = data[5] + ((int16_t)data[6]<<8);
+                            uint16_t flash_time = data[7] + ((int16_t)data[8]<<8);
+                            //Время не должно быть равно 0
+                            if(unlock_time != 0 || flash_time != 0){
+                                ESP_LOGI(RX_TASK_TAG, "Unlock_time: %d", unlock_time);
+                                ESP_LOGI(RX_TASK_TAG, "flash_time: %d", flash_time);
 
-                    if(data[4] == 1){
-
-
-                        //gptimer_stop(gptimer);
-                        //ESP_LOGI(RX_TASK_TAG, "Timer stop");
+                                //Создание задачи и передача в нее полученных параметров
+                                const struct channel_task_args args = {.channel = data[3], ._flash_time = flash_time, ._unlock_time = unlock_time};      
+                                xTaskCreate(task_channel, "task_channel", 1024*2, (void*)&args, configMAX_PRIORITIES-1, NULL);
+                            }    
+                            else ESP_LOGI(RX_TASK_TAG, "Time cannot be 0");
+                        }
+                        else ESP_LOGI(RX_TASK_TAG, "Command error");
                     }
-                    else if(data[4] == 2){
-                        
-                        uint16_t unlock_time = data[5] + ((int16_t)data[6]<<8);
-                        uint16_t flash_time = data[7] + ((int16_t)data[8]<<8);
-                        ESP_LOGI(RX_TASK_TAG, "Unlock_time: %d", unlock_time);
-                        ESP_LOGI(RX_TASK_TAG, "flash_time: %d", flash_time);
-
-                        //uint16_t args[] = {(uint16_t)data[3], unlock_time, flash_time};
-                        const struct channel_task_args args = {.channel = data[3], ._flash_time = flash_time, ._unlock_time = unlock_time};      
-                        //char *task_name = "task_channel_";
-                        //sprintf(task_name, "%d", data[3]);
-                        //const char *task_name_wtf = task_name;
-                         //args[data[3]].channel = data[3]; args[data[3]]._flash_time = flash_time; args[data[3]]._unlock_time = unlock_time;
-                        //ESP_LOGI(RX_TASK_TAG, task_name);
-
-                        xTaskCreate(task_channel, "task_channel", 1024*2, (void*)&args, configMAX_PRIORITIES-1, NULL);
-                        //gptimer_start(gptimer);
-                        //ESP_LOGI(RX_TASK_TAG, "Timer start");
-                    }
-                    else{
-                        ESP_LOGI(RX_TASK_TAG, "Command error");
-                    }
+                    else ESP_LOGI(RX_TASK_TAG, "Channel already work");                  
                 }
-                else{
-                    ESP_LOGI(RX_TASK_TAG, "Channel %d not found", data[3]);
-                }
-
-                
-                
-                
-            }
-            
+                else ESP_LOGI(RX_TASK_TAG, "Channel %d not found", data[3]);
+            }            
         }
     }
     free(data);
@@ -289,27 +267,8 @@ static void rx_task(void *arg)
 
 void app_main(void)
 {
+    //Инициализация
     init();
+    //Создание задачи приемника UART
     xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
-
-
 }
-
-
-//int sendData(const char* logName, const char* data)
-//{
-//    const int len = strlen(data);
-//    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
-//    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
-//    return txBytes;
-//}
-//
-//static void tx_task(void *arg)
-//{
-//    static const char *TX_TASK_TAG = "TX_TASK";
-//    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
-//    while (1) {
-//        sendData(TX_TASK_TAG, "Hello world");
-//        vTaskDelay(2000 / portTICK_PERIOD_MS);
-//    }
-//}
